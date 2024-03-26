ab3cf8596a6816ab490e7ea0127e22c98f2e35b4
shravyach17 <155272848+shravyach17@users.noreply.github.com>
2024-03-26 14:59:55 -0500

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "location" {
+  type        = string
+  description = "Name of the region for this infreastructure"
+  default     = "West US 2"
+}
+variable "network_address" {
+  type        = string
+  description = "Cidr range for the Virtual Network"
+  default     = "10.10.0.0/16"
+}
+variable "name" {
+  type        = string
+  description = "Name for this infrastructure"
+  default     = "rg"
+}
+
+
+
